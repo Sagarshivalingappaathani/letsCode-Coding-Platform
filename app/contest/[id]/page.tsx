93864f7c6cde02e0719168a188da999456eec42c
Sag@0418 <sagarathani0418@gmail.com>
2023-10-24 22:04:01 +0530

    solved the deployment error

--- app/contest/[id]/page.tsx
+++ app/contest/[id]/page.tsx
@@ -1,43 +1,11 @@
-"use client"
-import Contest from "../../../components/contestpage/Contestpage";
+// ./pages/contest/[id].tsx
+"use client";
+
 import { problems } from "../../../data/Problems/index";
 import { Problem } from "../../../data/types/problem";
-import React, { useState } from "react";
-import '../../../styles/global.css';
+import ProblemPage from "../../../components/contestpage/ProblemsPage";
 
-type ProblemPageProps = {
-  problem: Problem | null;
-};
-
-const ProblemPage: React.FC<ProblemPageProps> = ({ problem }) => {
-  const [proload, setproload] = useState(false);
-
-  if (!problem) {
-    return (
-      <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }} className="mt-60">
-        <span className="flex justify-center items-center loader"></span>
-      </div>
-    )
-  }
-
-  return (
-    <>
-      {proload &&
-        (
-          <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }} className="mt-60">
-            <span className="flex justify-center items-center loader"></span>
-          </div>
-        )
-      }
-      {
-        !proload && <Contest problem={problem} />
-      }
-
-    </>
-  );
-};
-
-export async function getStaticProps(context:any) {
+export async function getStaticProps(context: any) {
   const dynamicSegment = context.params.id; // Assuming you're using dynamic routing with Next.js
   const problemData = problems[dynamicSegment];
 
