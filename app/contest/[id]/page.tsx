17a109921069ce5d210e67358512a44ccb04f162
Sag@0418 <sagarathani0418@gmail.com>
2023-10-24 22:13:55 +0530

    solved the deployment error

--- app/contest/[id]/page.tsx
+++ app/contest/[id]/page.tsx
@@ -1,12 +1,51 @@
-// ./pages/contest/[id].tsx
-"use client";
-
+import { useEffect, useState } from "react";
+import Contest from "../../../components/contestpage/Contestpage";
 import { problems } from "../../../data/Problems/index";
 import { Problem } from "../../../data/types/problem";
-import ProblemPage from "../../../components/contestpage/ProblemsPage";
+import '../../styles/global.css';
 
-export async function getStaticProps(context: any) {
-  const dynamicSegment = context.params.id; // Assuming you're using dynamic routing with Next.js
+type ProblemPageProps = {
+  problem: Problem | null;
+};
+
+const ProblemPage: React.FC<ProblemPageProps> = ({ problem }) => {
+  const [proload, setproload] = useState(false);
+
+  useEffect(() => {
+    // You can also fetch additional data here if needed
+
+    if (!problem) {
+      setproload(true);
+
+      // Simulate a loading delay (remove this in production)
+      setTimeout(() => {
+        setproload(false);
+      }, 1000);
+    }
+  }, [problem]);
+
+  if (!problem) {
+    return (
+      <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }} className="mt-60">
+        <span className="flex justify-center items-center loader"></span>
+      </div>
+    );
+  }
+
+  return (
+    <>
+      {proload && (
+        <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }} className="mt-60">
+          <span className="flex justify-center items-center loader"></span>
+        </div>
+      )}
+      {!proload && <Contest problem={problem} />}
+    </>
+  );
+};
+
+export async function getServerSideProps(context: any) {
+  const dynamicSegment = context.params.id;
   const problemData = problems[dynamicSegment];
 
   if (!problemData) {
