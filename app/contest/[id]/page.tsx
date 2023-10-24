46e01f84a7e12d8ad50a6ecadfee16b91dfb12f8
Sag@0418 <sagarathani0418@gmail.com>
2023-10-24 22:17:19 +0530

    solved the deployment error

--- app/contest/[id]/page.tsx
+++ app/contest/[id]/page.tsx
@@ -1,3 +1,4 @@
+"use client"
 import { useEffect, useState } from "react";
 import Contest from "../../../components/contestpage/Contestpage";
 import { problems } from "../../../data/Problems/index";
