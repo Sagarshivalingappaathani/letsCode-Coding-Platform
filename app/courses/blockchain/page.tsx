ccc010b9b22e1c572ac3b0d4d1017d5ed124ed6a
Sag@0418 <sagarathani0418@gmail.com>
2023-11-08 19:57:23 +0530

    added the course PDFs

--- app/courses/blockchain/page.tsx
+++ app/courses/blockchain/page.tsx
@@ -1,25 +1,27 @@
 import React from 'react';
 
-type Props = {};
+const Blockchain: React.FC = () => {
+  const navbarHeight = 90; // Replace with the actual height of your navbar
 
-const Page = (props: Props) => {
   const containerStyle = {
-    height: 'calc(100vh - 90px)', // Adjust the navbar height as needed
+    height: `calc(100vh - ${navbarHeight}px)`,
     overflow: 'auto',
   };
 
-  const iframeStyle = {
-    width: '100%',
-    height: '100%',
-    maxWidth: '100%',
-  };
-
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-4xl text-red-600 font-semibold">Work in Progress</h1>
-      <p className="text-lg text-gray-600">This page is under construction and will be available soon.</p>
+    <div style={containerStyle}>
+      <iframe
+        src="/assets/CourseData/Blockchain.pdf"
+        title="PDF Page"
+        style={{
+          width: '100%',
+          height: '100%',
+          border: 'none',
+          backgroundColor: 'white' // Remove iframe border
+        }}
+      />
     </div>
   );
 };
 
-export default Page;
+export default Blockchain;
